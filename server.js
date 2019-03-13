6b0ffd081815a74b7c8389dfc7c5c2cfca5b6679
WAHHAB BALDWIN <wahhabb@gmail.com>
2019-03-12 17:40:19 -0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ app.use((req, res, next) => {
 	});
 	next();
 });
-// app.use((req, res, next) => {
-// 	res.render('maintenance.hbs');
-// });
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -48,6 +45,7 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Server is up on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
 });
